61660b123ebbb67133e6d440d8cb1c00f0868871
Mingyu Cheon <79088638+Martinj-31@users.noreply.github.com>
2022-12-02 17:03:29 +0900

    Update pca_lda.py
    Import MNIST dataset library

--- pca_lda.py
+++ pca_lda.py
@@ -1,29 +1,14 @@
-import os
-import struct
 import numpy as np
 import numpy.linalg as lin
+import MNISTdataset as MNIST
+
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 
 #Load the data
-def load_mnist(path, kind='train'):
-    labels_path = os.path.join(path, '%s-labels.idx1-ubyte' %kind)
-    images_path = os.path.join(path, '%s-images.idx3-ubyte' %kind)
-
-    with open(labels_path, 'rb') as lbpath:
-        magic, n = struct.unpack('>II', lbpath.read(8))
-        labels = np.fromfile(lbpath, dtype=np.uint8)
-
-    with open(images_path, 'rb') as imgpath:
-        magic, num, rows, cols = struct.unpack('>IIII', imgpath.read(16))
-        images = np.fromfile(imgpath, dtype=np.uint8).reshape(len(labels), 784)
-
-    return images, labels
-
-train_x, train_y = load_mnist('/Users/mingyucheon/Desktop/dataset', kind='train')
-print(len(train_x))
-test_x, test_y = load_mnist('/Users/mingyucheon/Desktop/dataset', kind='t10k')
+train_x, train_y = MNIST.load_mnist('/Users/mingyucheon/Desktop/dataset', kind='train')
+test_x, test_y = MNIST.load_mnist('/Users/mingyucheon/Desktop/dataset', kind='t10k')
 train_x = train_x.reshape(-1, 28*28)
 
 # Standardize the feature matrix
